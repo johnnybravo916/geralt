353780c10539200acfb25a40a7d2bae9b50534af
johnnybravo916 <johnong916@gmail.com>
2017-02-09 00:57:28 -0600

    slick

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,10 @@ function getYear(){
 
 $(document).ready(function() {
 //Call functions here
-responsiveClasses();
-getYear();
+  responsiveClasses();
+  getYear();
+//SLICK SLIDER
+  $(".regular").slick({
+    //parameters
+  });
 });
